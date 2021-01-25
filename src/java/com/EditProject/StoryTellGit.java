054fa0e261c7471835a450f73fbfa830feade56b
Sukma Hasnaa <shasukma@gmail.com>
2021-01-25 21:36:25 +0700

    EditProject

---
+++ src/java/com/EditProject/StoryTellGit.java
@@ -0,0 +1,14 @@
+package com.EditProject;
+// Generated Jan 25, 2021 11:10:05 AM by Hibernate Tools 4.3.1
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class StoryTellGit { 
+ // Group 5
+// Putri Nabila Hapsari
+// Sukma Hasnaa Ambaroh
+//  This project is made to make it easier for users to write their diaries which can be shared and then the story can viewed and like by other users
+}
